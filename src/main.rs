71b10a663631d8475813146fafb8bd5d2d647170
Tuupertunut <tuupertunut@outlook.com>
2021-11-13 20:34:50 +0200

    Make board parsing stricter

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,10 @@ fn main() {
 }
 
 fn parse_board(input: &str) -> Result<Vec<Vec<Tile>>, Box<dyn Error>> {
-    let mut rows = Vec::<Vec<Tile>>::new();
+    let mut board = Vec::<Vec<Tile>>::new();
 
     for row_string in input.trim_end().split("\n") {
-        let mut tiles = Vec::<Tile>::new();
+        let mut row = Vec::<Tile>::new();
 
         /* Splitting row into 4 character pieces. */
         for tile_string in row_string
@@ -32,27 +32,27 @@ fn parse_board(input: &str) -> Result<Vec<Vec<Tile>>, Box<dyn Error>> {
             .chunks(4)
             .map(String::from_utf8_lossy)
         {
-            let tile_content = tile_string.trim();
+            let tile_content = tile_string.trim_end();
 
             if tile_content == "" {
-                tiles.push(Tile::NoTile);
-            } else if tile_content == "0" {
-                tiles.push(Tile::Empty);
+                row.push(Tile::NoTile);
+            } else if tile_content == " 0" {
+                row.push(Tile::Empty);
             } else if tile_content.starts_with("+") {
                 let stack_size = tile_content[1..].parse::<u8>()?;
-                tiles.push(Tile::MaxStack(stack_size));
+                row.push(Tile::MaxStack(stack_size));
             } else if tile_content.starts_with("-") {
                 let stack_size = tile_content[1..].parse::<u8>()?;
-                tiles.push(Tile::MinStack(stack_size));
+                row.push(Tile::MinStack(stack_size));
             } else {
                 return Err("Invalid tile")?;
             }
         }
 
-        rows.push(tiles);
+        board.push(row);
     }
 
-    return Ok(rows);
+    return Ok(board);
 }
 
 fn write_board(board: &Vec<Vec<Tile>>) -> String {
