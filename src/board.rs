98dd92430092b8a040b199bd5624190b4a3c82b5
Tuupertunut <tuupertunut@outlook.com>
2021-11-16 04:15:45 +0200

    Refactor board functions into a separate module

---
+++ src/board.rs
@@ -0,0 +1,152 @@
+use std::error::Error;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Player {
+    Min,
+    Max,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Tile {
+    NoTile, /* outside of the board */
+    Empty,
+    Stack(Player, u8),
+}
+
+/* Coordinate offsets for each neighbor in a hex grid. Neighbors can be found by adding these to our
+ * current coordinates. These also represent straight line directions. */
+pub const NEIGHBOR_OFFSETS: [(isize, isize); 6] =
+    [(0, 1), (1, 1), (1, 0), (0, -1), (-1, -1), (-1, 0)];
+
+pub struct Board(Vec<Vec<Tile>>);
+
+impl Board {
+    pub fn parse(input: &str) -> Result<Board, Box<dyn Error>> {
+        let mut board = Vec::<Vec<Tile>>::new();
+
+        for row_string in input.trim_end().split("\n") {
+            let mut row = Vec::<Tile>::new();
+
+            /* Splitting row into 4 character pieces. */
+            for tile_string in row_string
+                .trim_end()
+                .as_bytes()
+                .chunks(4)
+                .map(String::from_utf8_lossy)
+            {
+                let tile_content = tile_string.trim_end();
+
+                if tile_content == "" {
+                    row.push(Tile::NoTile);
+                } else if tile_content == " 0" {
+                    row.push(Tile::Empty);
+                } else if tile_content.starts_with("+") {
+                    let stack_size = tile_content[1..].parse::<u8>()?;
+                    row.push(Tile::Stack(Player::Max, stack_size));
+                } else if tile_content.starts_with("-") {
+                    let stack_size = tile_content[1..].parse::<u8>()?;
+                    row.push(Tile::Stack(Player::Min, stack_size));
+                } else {
+                    return Err("Invalid tile")?;
+                }
+            }
+
+            board.push(row);
+        }
+
+        return Ok(Board(board));
+    }
+
+    pub fn write(&self) -> String {
+        let Board(board) = self;
+
+        let mut output = String::new();
+
+        for row in board {
+            for &tile in row {
+                let tile_string = match tile {
+                    Tile::NoTile => format!("    "),
+                    Tile::Empty => format!(" 0  "),
+                    Tile::Stack(Player::Max, stack_size) => format!("+{:<3}", stack_size),
+                    Tile::Stack(Player::Min, stack_size) => format!("-{:<3}", stack_size),
+                };
+                output.push_str(&tile_string);
+            }
+            output.push_str("\n");
+        }
+
+        return output;
+    }
+
+    pub fn possible_moves(&self, player: Player) -> Vec<Board> {
+        let Board(board) = self;
+
+        let mut next_boards = Vec::<Board>::new();
+
+        /* r = row coordinate, q = column coordinate */
+        for (from_r, row) in board.iter().enumerate() {
+            for (from_q, &tile) in row.iter().enumerate() {
+                if let Tile::Stack(p, size) = tile {
+                    if p == player && size > 1 {
+                        /* Loop through all straight line directions. */
+                        for (dir_r, dir_q) in NEIGHBOR_OFFSETS {
+                            /* Move to a direction as far as there are empty tiles. */
+                            let mut r = from_r;
+                            let mut q = from_q;
+                            loop {
+                                /* Coordinates for the next tile in the direction.
+                                 * Hack: negative numbers cannot be added to a usize, so they are
+                                 * converted into usize with underflow and then added with overflow.
+                                 * Same as: let next_r = r + dir_r */
+                                let next_r = r.wrapping_add(dir_r as usize);
+                                let next_q = q.wrapping_add(dir_q as usize);
+
+                                /* If next tile is empty, move to that tile. */
+                                if next_r < board.len()
+                                    && next_q < board[next_r].len()
+                                    && board[next_r][next_q] == Tile::Empty
+                                {
+                                    r = next_r;
+                                    q = next_q;
+                                } else {
+                                    break;
+                                }
+                            }
+
+                            /* Check if we actually found any empty tiles in the direction. */
+                            if r != from_r || q != from_q {
+                                for split in 1..size {
+                                    let mut next_board = board.clone();
+                                    next_board[r][q] = Tile::Stack(player, split);
+                                    next_board[from_r][from_q] = Tile::Stack(player, size - split);
+                                    next_boards.push(Board(next_board));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return next_boards;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn output_equals_input() {
+        let input = " 0  +2       0  \
+                   \n-1   0  -3  +2  \
+                   \n     0  \
+                   \n";
+        assert_eq!(input, Board::parse(input).unwrap().write());
+    }
+
+    #[test]
+    fn parse_fails_on_invalid_board() {
+        assert!(Board::parse("abcdefg").is_err());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-use std::error::Error;
+mod board;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum Tile {
-    NoTile, /* outside of the board */
-    Empty,
-    MaxStack(u8),
-    MinStack(u8),
-}
+use board::*;
 
 fn main() {
     println!("Enter a board");
@@ -16,135 +10,10 @@ fn main() {
             .read_line(&mut input_buffer)
             .expect("Input contained illegal characters");
     }
-    let board = parse_board(&input_buffer).expect("Input is not a valid board");
-    println!("{}", write_board(&board));
+    let board = Board::parse(&input_buffer).expect("Input is not a valid board");
+    println!("{}", board.write());
     println!("possible moves");
-    for next_board in possible_moves(&board) {
-        println!("{}", write_board(&next_board));
-    }
-}
-
-fn possible_moves(board: &Vec<Vec<Tile>>) -> Vec<Vec<Vec<Tile>>> {
-    let mut next_boards = Vec::<Vec<Vec<Tile>>>::new();
-
-    /* r = row coordinate, q = column coordinate */
-    for (from_r, row) in board.iter().enumerate() {
-        for (from_q, tile) in row.iter().enumerate() {
-            if let Tile::MaxStack(size) = tile {
-                if *size > 1 {
-                    /* Loop through all straight line directions. */
-                    for (dir_r, dir_q) in [(0, 1), (1, 1), (1, 0), (0, -1), (-1, -1), (-1, 0)] {
-                        /* Move to a direction as far as there are empty tiles. */
-                        let mut r = from_r;
-                        let mut q = from_q;
-                        loop {
-                            /* Coordinates for the next tile in the direction.
-                             * Hack: negative numbers cannot be added to a usize, so they are
-                             * converted into usize with underflow and then added with overflow.
-                             * Same as: let next_r = r + dir_r */
-                            let next_r = r.wrapping_add(dir_r as usize);
-                            let next_q = q.wrapping_add(dir_q as usize);
-
-                            /* If next tile is empty, move to that tile. */
-                            if next_r < board.len()
-                                && next_q < board[next_r].len()
-                                && board[next_r][next_q] == Tile::Empty
-                            {
-                                r = next_r;
-                                q = next_q;
-                            } else {
-                                break;
-                            }
-                        }
-
-                        /* Check if we actually found any empty tiles in the direction. */
-                        if r != from_r || q != from_q {
-                            for split in 1..*size {
-                                let mut next_board = board.clone();
-                                next_board[r][q] = Tile::MaxStack(split);
-                                next_board[from_r][from_q] = Tile::MaxStack(size - split);
-                                next_boards.push(next_board);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    return next_boards;
-}
-
-fn parse_board(input: &str) -> Result<Vec<Vec<Tile>>, Box<dyn Error>> {
-    let mut board = Vec::<Vec<Tile>>::new();
-
-    for row_string in input.trim_end().split("\n") {
-        let mut row = Vec::<Tile>::new();
-
-        /* Splitting row into 4 character pieces. */
-        for tile_string in row_string
-            .trim_end()
-            .as_bytes()
-            .chunks(4)
-            .map(String::from_utf8_lossy)
-        {
-            let tile_content = tile_string.trim_end();
-
-            if tile_content == "" {
-                row.push(Tile::NoTile);
-            } else if tile_content == " 0" {
-                row.push(Tile::Empty);
-            } else if tile_content.starts_with("+") {
-                let stack_size = tile_content[1..].parse::<u8>()?;
-                row.push(Tile::MaxStack(stack_size));
-            } else if tile_content.starts_with("-") {
-                let stack_size = tile_content[1..].parse::<u8>()?;
-                row.push(Tile::MinStack(stack_size));
-            } else {
-                return Err("Invalid tile")?;
-            }
-        }
-
-        board.push(row);
-    }
-
-    return Ok(board);
-}
-
-fn write_board(board: &Vec<Vec<Tile>>) -> String {
-    let mut output = String::new();
-
-    for row in board {
-        for tile in row {
-            let tile_string = match tile {
-                &Tile::NoTile => format!("    "),
-                &Tile::Empty => format!(" 0  "),
-                &Tile::MaxStack(stack_size) => format!("+{:<3}", stack_size),
-                &Tile::MinStack(stack_size) => format!("-{:<3}", stack_size),
-            };
-            output.push_str(&tile_string);
-        }
-        output.push_str("\n")
-    }
-
-    return output;
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn output_equals_input() {
-        let input = " 0  +2       0  \
-                   \n-1   0  -3  +2  \
-                   \n     0  \
-                   \n";
-        assert_eq!(input, write_board(&parse_board(input).unwrap()));
-    }
-
-    #[test]
-    fn parse_fails_on_invalid_board() {
-        assert!(parse_board("abcdefg").is_err());
+    for next_board in board.possible_moves(Player::Max) {
+        println!("{}", next_board.write());
     }
 }
