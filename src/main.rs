922894ea7c0f7016560279686f86f9b2fe59060b
Tuupertunut <tuupertunut@outlook.com>
2021-11-22 04:45:16 +0200

    Implement minimax algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod board;
 
+use std::time::Instant;
+
 use board::*;
 
 fn main() {
@@ -10,12 +12,118 @@ fn main() {
             .read_line(&mut input_buffer)
             .expect("Input contained illegal characters");
     }
-    let board = Board::parse(&input_buffer).expect("Input is not a valid board");
-    println!("{}", board.write());
-    println!("{}", board.evaluate());
-    println!("possible moves");
-    for next_board in board.possible_moves(Player::Max) {
-        println!("{}", next_board.write());
-        println!("{}", next_board.evaluate());
+    let mut board = Board::parse(&input_buffer).expect("Input is not a valid board");
+
+    for &player in [Player::Min, Player::Max].iter().cycle() {
+        let start_time = Instant::now();
+        let (next_board, value, count) = match player {
+            Player::Min => min_choose(&board, |board_1| {
+                max_value(board_1, |board_2| {
+                    min_value(board_2, |board_3| {
+                        max_value(board_3, |board_4| (board_4.evaluate(), 1))
+                    })
+                })
+            }),
+            Player::Max => max_choose(&board, |board_1| {
+                min_value(board_1, |board_2| {
+                    max_value(board_2, |board_3| {
+                        min_value(board_3, |board_4| (board_4.evaluate(), 1))
+                    })
+                })
+            }),
+        };
+        match next_board {
+            None => break,
+            Some(next_board) => {
+                println!(
+                    "took {:?}, evaluated {} boards, value {}\n{}",
+                    start_time.elapsed(),
+                    count,
+                    value,
+                    next_board.write()
+                );
+                board = next_board;
+            }
+        }
     }
+}
+
+fn min_value<F>(board: &Board, max_value_function: F) -> (i32, u64)
+where
+    F: Fn(&Board) -> (i32, u64),
+{
+    let mut value = i32::MAX;
+    let mut count = 0;
+    for next_board in board.possible_moves(Player::Min) {
+        let (next_board_value, next_board_count) = max_value_function(&next_board);
+        value = i32::min(value, next_board_value);
+        count += next_board_count;
+    }
+    if value == i32::MAX {
+        value = board.evaluate();
+        count += 1;
+    }
+    return (value, count);
+}
+
+fn max_value<F>(board: &Board, min_value_function: F) -> (i32, u64)
+where
+    F: Fn(&Board) -> (i32, u64),
+{
+    let mut value = i32::MIN;
+    let mut count = 0;
+    for next_board in board.possible_moves(Player::Max) {
+        let (next_board_value, next_board_count) = min_value_function(&next_board);
+        value = i32::max(value, next_board_value);
+        count += next_board_count;
+    }
+    if value == i32::MIN {
+        value = board.evaluate();
+        count += 1;
+    }
+    return (value, count);
+}
+
+fn min_choose<F>(board: &Board, max_value_function: F) -> (Option<Board>, i32, u64)
+where
+    F: Fn(&Board) -> (i32, u64),
+{
+    let mut value = i32::MAX;
+    let mut chosen_move = None;
+    let mut count = 0;
+    for next_board in board.possible_moves(Player::Min) {
+        let (next_board_value, next_board_count) = max_value_function(&next_board);
+        if next_board_value < value {
+            value = next_board_value;
+            chosen_move = Some(next_board);
+        }
+        count += next_board_count;
+    }
+    if value == i32::MAX {
+        value = board.evaluate();
+        count += 1;
+    }
+    return (chosen_move, value, count);
+}
+
+fn max_choose<F>(board: &Board, min_value_function: F) -> (Option<Board>, i32, u64)
+where
+    F: Fn(&Board) -> (i32, u64),
+{
+    let mut value = i32::MIN;
+    let mut chosen_move = None;
+    let mut count = 0;
+    for next_board in board.possible_moves(Player::Max) {
+        let (next_board_value, next_board_count) = min_value_function(&next_board);
+        if next_board_value > value {
+            value = next_board_value;
+            chosen_move = Some(next_board);
+        }
+        count += next_board_count;
+    }
+    if value == i32::MIN {
+        value = board.evaluate();
+        count += 1;
+    }
+    return (chosen_move, value, count);
 }
